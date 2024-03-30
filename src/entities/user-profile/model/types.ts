a6d5260bc4d3ffca347b73099f8403f9b38e2fd6
ZilioPoilo <frolyakyurii@gmail.com>
2024-03-30 22:05:23 +0200

    Add types

--- src/entities/user-profile/model/types.ts
+++ src/entities/user-profile/model/types.ts
@@ -1,15 +1,30 @@
-export interface IUserProfile {
+import { IWishList } from "@/entities/wish-list";
 
+export interface IUserProfile {
+    Id: number;
+    Name: string;
+    Email: string;
+    AvatarUrl: string;
+    AvatarPath: string;
+    WishList: IWishList[];
+    HistoryList: IHistory[];
+    NotificationList: INotification[];
+    IsPrivate: boolean;
 }
 
 export interface IUserProfilePreview {
-
+    Id:string;
+    Name:string;
+    AvatarUrl:string;
+    AvatarPath:string;
 }
 
 export interface IHistory {
-
+    ContentId: string;
+    ViewDate:Date;
 }
 
 export interface INotification {
-
+    Message:string;
+    LinkRaw:string;
 }
--- src/entities/wish-list/model/types.ts
+++ src/entities/wish-list/model/types.ts
@@ -1,7 +1,12 @@
 export interface IWishList {
-
+    Id:string;
+    Name:string;
+    IsPrivate:boolean;
+    Notify:boolean;
+    ContentList:IWishListContent[];
 }
 
 export interface IWishListContent {
-
+    ContentId:string;
+    Priority:number;
 }
