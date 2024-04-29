af656f9edb3bc4ed75e9bce3b7db14fdd06f5d89
ZilioPoilo <frolyakyurii@gmail.com>
2024-04-29 17:52:00 +0300

    Add Profile Comments page

---
+++ src/features/profile-comment/ui/profile-comment/profile-comment.tsx
@@ -0,0 +1,28 @@
+import {FC} from 'react';
+import {FlexContainer, Paragraph, Button} from "@/shared/ui-kit";
+import styles from "./profile-comment.module.scss";
+import {IComment} from "@/entities/comment";
+import {commonApi} from "@/entities/common";
+
+
+const ProfileComment: FC<{comment: IComment}> = ({comment}) => {
+  const data = commonApi.useFetchByIdQuery(comment.contentId!);
+
+
+  return (
+    <FlexContainer className={styles.commentCard} justify="space-between" align="end" gap={10}>
+      <FlexContainer styles={{padding: 10}} gap={10} vertical>
+        <FlexContainer>
+          <Paragraph color="primary" >{data.data?.name}</Paragraph>
+          <Paragraph color="secondary" >{comment.dispatch}</Paragraph>
+        </FlexContainer>
+        <Paragraph color="secondary">{comment.message}</Paragraph>
+      </FlexContainer>
+      <FlexContainer className={styles.forButton} gap={10} vertical>
+        <Button size="small" text="Перейти до" />
+      </FlexContainer>
+    </FlexContainer>
+  );
+};
+
+export default ProfileComment;
