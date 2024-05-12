674964983181270964a2f470a6cc8bb17609318b
Navatusein <boghdan.kutsulima@gmail.com>
2024-05-12 14:55:50 +0300

    Fix Profile Comment

--- src/features/profile-comment/ui/profile-comment/profile-comment.tsx
+++ src/features/profile-comment/ui/profile-comment/profile-comment.tsx
@@ -3,10 +3,20 @@ import {FlexContainer, Paragraph, Button} from "@/shared/ui-kit";
 import styles from "./profile-comment.module.scss";
 import {IComment} from "@/entities/comment";
 import {previewApi} from "@/entities/common";
+import {useNavigate} from "react-router-dom";
 
+const contentTypeToString = ["movie", "serial", "cartoon", "anime"];
 
 const ProfileComment: FC<{comment: IComment}> = ({comment}) => {
+  const navigate = useNavigate();
   const data = previewApi.useFetchByIdQuery(comment.contentId!);
+
+  const onClick = () => {
+    if (data.data == undefined)
+      return;
+
+    navigate(`/content/${contentTypeToString[data.data.contentType - 1]}/${data.data.id}#comments`);
+  }
 
   return (
     <FlexContainer className={styles.commentCard} justify="space-between" align="center" gap={10}>
@@ -18,7 +28,7 @@ const ProfileComment: FC<{comment: IComment}> = ({comment}) => {
         <Paragraph color="secondary">{comment.message}</Paragraph>
       </FlexContainer>
       <FlexContainer className={styles.forButton} justify="end" gap={10} vertical>
-        <Button size="small" text="Перейти до" />
+        <Button size="small" text="Перейти до" onClick={() => onClick()}/>
       </FlexContainer>
     </FlexContainer>
   );
